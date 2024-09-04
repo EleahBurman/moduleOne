305d75e9a0566f20e4e38dd060b55adf19a7caed
eleahburman <eleahjburman@gmail.com>
2024-09-04 15:58:01 -0400

    Showing concatenation

--- hello.py
+++ hello.py
@@ -2,14 +2,17 @@
 print("Aloha, Eleah")
 
 #showing that a number is a number vs a string is a string
-x=5
+x = 5
 print(x)
 print("Aloha", x)
 
 #showing how modulo work with the resulting value being the remainder
-x=5%2
+x = 5 % 2
 print("this is the modulo - ", x)
 
 #explaining floor and going to the lowest value
-x=5//2
+x = 5 // 2
 print("this is floor - ", x)
+
+#showing concatenation
+print("Aloha " + "World")
