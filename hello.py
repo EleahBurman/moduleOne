dde95b643bc6b63afe576acf07d303841044c3d4
eleahburman <eleahjburman@gmail.com>
2024-09-04 15:48:17 -0400

    Showing how floor and modulo work

--- hello.py
+++ hello.py
@@ -5,3 +5,11 @@ print("Aloha, Eleah")
 x=5
 print(x)
 print("Aloha", x)
+
+#showing how modulo work with the resulting value being the remainder
+x=5%2
+print("this is the modulo - ", x)
+
+#explaining floor and going to the lowest value
+x=5//2
+print("this is floor - ", x)
