7b8aa415fb77bfd1568ec1a9b6a82545aa0421f1
eleahburman <eleahjburman@gmail.com>
2024-09-04 16:22:22 -0400

    Showing a function with f strings

--- hello.py
+++ hello.py
@@ -24,3 +24,19 @@ print("Aloha" * 5)
 print("Aloha World ++++")
 plus = "+" * 4
 print(f"Aloha World {plus}")
+
+#showing functions - put in a number return a string
+def aloha_chars(number: int) -> str:
+  """
+  this aloha takes aloha plus '+' signs equals to number.
+  examples aloha_chars(5), "aloha +++++"
+  args:
+    number(int): numbers of pluses i need
+    returns: str: aloha plus pluses
+  """
+  pluses = '+' * number
+  return f"Aloha {pluses}"
+
+#showing the function working
+print("pluses is 5 - ", aloha_chars(5))
+print("pluses is 10 - ", aloha_chars(10))
